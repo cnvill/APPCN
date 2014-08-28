a025318f3c59b91b399f07f64294800e4a1cd011
CN <CN@CRISTIAN>
2014-08-27 22:55:54 -0500

    Agregar campo get estado Signed-off-by:CN <CN@CRISTIAN>

---
+++ src/java/BL/BLDetMatricula.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BL;
+
+/**
+ *
+ * @author CN
+ */
+public class BLDetMatricula {
+    
+}
